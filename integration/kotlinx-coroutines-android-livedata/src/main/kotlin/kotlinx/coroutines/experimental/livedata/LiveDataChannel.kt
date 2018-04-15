59f2d04c2e366c00098e10117bee8880ee047d32
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2018-04-15 18:15:28 +1000

    Added live data integration module

---
+++ integration/kotlinx-coroutines-android-livedata/src/main/kotlin/kotlinx/coroutines/experimental/livedata/LiveDataChannel.kt
@@ -0,0 +1,36 @@
+package kotlinx.coroutines.experimental.livedata
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.experimental.channels.LinkedListChannel
+import kotlinx.coroutines.experimental.channels.SubscriptionReceiveChannel
+
+class LiveDataChannel<T>(private val liveData: LiveData<T>)
+    : LinkedListChannel<T?>(), SubscriptionReceiveChannel<T?>, Observer<T?>, LifecycleObserver {
+
+    override fun onChanged(t: T?) {
+        offer(t)
+    }
+
+    override fun afterClose(cause: Throwable?) = liveData.removeObserver(this)
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() = close()
+
+}
+
+fun <T> LiveData<T>.observeChannel(lifecycleOwner: LifecycleOwner): LiveDataChannel<T> {
+    val channel = LiveDataChannel(this)
+    observe(lifecycleOwner, channel)
+    lifecycleOwner.lifecycle.addObserver(channel)
+    return channel
+}
+
+fun <T> LiveData<T>.observeChannel(): LiveDataChannel<T> {
+    val channel = LiveDataChannel(this)
+    observeForever(channel)
+    return channel
+}
